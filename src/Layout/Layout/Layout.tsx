e1dd95a21ac822028a5b1ed3abf582ff0a91738b
roqzych <roqzych@gmail.com>
2022-10-16 15:22:21 +0300

    Слайдер внутри компонента - тоже плохо

--- src/Layout/Layout/Layout.tsx
+++ src/Layout/Layout/Layout.tsx
@@ -3,63 +3,55 @@ import css from "./Layout.module.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 // import { Outlet } from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
 import Homepage from "../../Pages/Homepage/Homepage";
+import pink1 from "../../assets/sprites/pink1.png";
+import bigBacteria from "../../assets/sprites/bigBacteria.png";
 
 const Layout = () => {
-    let x1: any;
-    let y1: any;
-    const handleTouchStart = (e: any) => {
-        let firstTouch = e.touches[0];
-        // console.log(firstTouch);
+    // let x1: any;
+    // let y1: any;
+    // const handleTouchStart = (e: any) => {
+    //     let firstTouch = e.touches[0];
+    //     x1 = firstTouch.clientX;
+    //     y1 = firstTouch.clientY;
+    // };
 
-        x1 = firstTouch.clientX;
-        y1 = firstTouch.clientY;
-        // console.log(x1, y1);
-    };
-
-    const handleTouchMove = (e: any) => {
-        if (!x1 || !y1) {
-            return false;
-        }
-
-        let x2 = e.touches[0].clientX;
-        // let y2 = e.touches[0].clientX;
-        // console.log(x2, y2);
-
-        let difLeft = x1 - x2;
-        let difRight = x2 - x1;
-
-        console.log(difRight);
-        if (difLeft > 200) {
-            swipeContent("right");
-        }
-        if (difRight > 200) {
-            swipeContent("left");
-        }
-    };
-
-    const [currentPage, changePage] = useState(1);
-
-    const swipeContent = (side: string) => {
-        if (side === "right") {
-            changePage(2);
-        }
-        if (side === "left") {
-            changePage(1);
-        }
-    };
+    // const handleTouchMove = (e: any) => {
+    //     if (!x1 || !y1) {
+    //         return false;
+    //     }
+    //     let x2 = e.touches[0].clientX;
+    //     let difLeft = x1 - x2;
+    //     let difRight = x2 - x1;
+    //     // console.log(difRight);
+    //     if (difLeft > 200) {
+    //         swipeContent("right");
+    //     }
+    //     if (difRight > 200) {
+    //         swipeContent("left");
+    //     }
+    // };
 
     return (
         <div className={css.body}>
             <Header />
 
-            <main className={css.main} onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
-                <Homepage currentPage={currentPage} />
+            <main className={css.main}>
+                <Homepage />
                 {/* <Outlet currentPage={currentPage}/> */}
             </main>
 
             <Footer />
+
+            <div className={css.sprites}>
+                <div className={css.sprites__pink1}>
+                    <img src={pink1} alt="purple1" />
+                </div>
+                <div className={css.sprites__bigBacteria}>
+                    <img className={css.animateBlock} src={bigBacteria} alt="bigBacteria" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -3,43 +3,110 @@
 import css from "./Homepage.module.css";
 
 import buttonNext from "../../assets/sprites/buttonNext.png";
-import pink1 from "../../assets/sprites/pink1.png";
-import bigBacteria from "../../assets/sprites/bigBacteria.png";
-// import classNames from "classnames";
+import cn from "classnames";
+import { useRef, useState } from "react";
 
-const Homepage = (props: any) => {
-    // const setActive = ({ isActive: any }) => (isActive ? css.profileActive : css.profileInactive);
+// import pink1 from "../../assets/sprites/pink1.png";
+// import bigBacteria from "../../assets/sprites/bigBacteria.png";
 
-    // const homepage = document.querySelector(".home");
-    // document.querySelector(".homepage")?.addEventListener('click', function() {
+const Homepage = () => {
+    let x1: any;
+    let y1: any;
+    const handleTouchStart = (e: any) => {
+        let firstTouch = e.touches[0];
+        x1 = firstTouch.clientX;
+        y1 = firstTouch.clientY;
+    };
 
-    //     homepage.style.left = "-1024px"
-    // })
+    const handleTouchMove = (e: any) => {
+        if (!x1 || !y1) {
+            return false;
+        }
+        let x2 = e.touches[0].clientX;
+        let difLeft = x1 - x2;
+        let difRight = x2 - x1;
+        // console.log(difRight);
+        if (difLeft > 200) {
+            swipeContent("right");
+        }
+        if (difRight > 200) {
+            swipeContent("left");
+        }
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [currentPage, changePage] = useState(1);
+
+    const swipeContent = (side: string) => {
+        if (side === "right") {
+            nextGo();
+        }
+        if (side === "left") {
+            lastGo();
+        }
+        console.log("сработало");
+    };
+
+    const appBody = useRef<HTMLDivElement>(null);
+
+    const nextGo = () => {
+        // console.log(appBody);
+        appBody?.current?.childNodes.forEach((element: any) => {
+            element.style = "transform: translateX(-1024px)";
+            element.background = "transform: translateX(-1024px)";
+        });
+    };
+    const lastGo = () => {
+        appBody?.current?.childNodes.forEach((element: any) => {
+            element.style = "transform: translateX(0px)";
+            element.background = "transform: translateX(0px)";
+        });
+    };
     return (
-        <div className={props.currentPage === 1 ? css.homepage : css.helppage}>
-            {/* <div className={css.homepage + "home"}> */}
-            <div className={css.homepage__hello}>ПРИВЕТ,</div>
-            <div className={css.homepage__text}>
-                <div>
-                    {/* <p> */}
-                    ЭТО <b> НЕ </b> КОММЕРЧЕСКОЕ ЗАДАНИЕ
-                    <img src={buttonNext} alt="purple button next" />
-                    {/* </p> */}
+        // <div className={props.currentPage === 1 ? css.homepage : css.helppage}>
+        <div className={css.appBody} onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
+            {/* <button onClick={nextGo}>след</button>
+            <button onClick={lastGo}>след</button> */}
+
+            {/* <button onClick={() => swipeContent("right")}>СЛЕДУЮЩАЯ</button>
+            <button onClick={() => swipeContent("left")}>предыдущая</button> */}
+            <div className={css.appBodyTrack} ref={appBody}>
+                <div className={cn(css.appBody__homepage, css.homepage)}>
+                    <div className={css.homepage__hello}>ПРИВЕТ,</div>
+                    <div className={css.homepage__text}>
+                        <div>
+                            {/* <p> */}
+                            ЭТО <b> НЕ </b> КОММЕРЧЕСКОЕ ЗАДАНИЕ
+                            <img src={buttonNext} alt="purple button next" onClick={() => nextGo()} />
+                            {/* </p> */}
+                        </div>
+                        {/* <div className={css.sprites}>
+                            <div className={css.sprites__pink1}>
+                                <img src={pink1} alt="purple1" />
+                            </div>
+                            <div className={css.sprites__bigBacteria}>
+                                <img className={css.animateBlock} src={bigBacteria} alt="bigBacteria" />
+                            </div>
+                        </div> */}
+                    </div>
                 </div>
+                <div className={cn(css.appBody__homepage, css.homepage)}>
+                    <div className={css.homepage__hello}>ПРИВЕТ,</div>
+                    <div className={css.homepage__text}>
+                        <div>
+                            ЭТО <b> НЕ </b> КОММЕРЧЕСКОЕ ЗАДАНИЕ
+                            <img src={buttonNext} alt="purple button next" />
+                        </div>
+                    </div>
 
-                {/* <img src={buttonNext} alt="purple button next" /> */}
-            </div>
-
-            {/* <div className={css.homepage__pink1}>
-                <img src={pink1} alt="purple1" />
-            </div> */}
-
-            <div className={css.sprites}>
-                <div className={css.sprites__pink1}>
-                    <img src={pink1} alt="purple1" />
-                </div>
-                <div className={css.sprites__bigBacteria}>
-                    <img className={css.animateBlock} src={bigBacteria} alt="bigBacteria" />
+                    {/* <div className={css.sprites}>
+                        <div className={css.sprites__pink1}>
+                            <img src={pink1} alt="purple1" />
+                        </div>
+                        <div className={css.sprites__bigBacteria}>
+                            <img className={css.animateBlock} src={bigBacteria} alt="bigBacteria" />
+                        </div>
+                    </div> */}
                 </div>
             </div>
         </div>
